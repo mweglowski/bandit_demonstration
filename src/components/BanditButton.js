fd6d214d3f3f9016d6a1db367ebc094bf51a9bc9
Marcin Weglowski <marcin.weglowski1@gmail.com>
2024-02-27 12:24:50 +0100

    button design

--- src/components/BanditButton.js
+++ src/components/BanditButton.js
@@ -7,7 +7,7 @@ const BanditButton = ({ onButtonClick, data }) => {
 
   return (
     <div
-      className="rounded-lg mx-2 border-[3px] border-emerald-800 h-[5em] w-[5em] shadow-md shadow-emerald-800 hover:cursor-pointer hover:bg-emerald-700 hover:shadow-lg hover:shadow-emerald-800 duration-300"
+      className="rounded-lg mx-2 border-[3px] border-teal-700 h-[5em] w-[5em] shadow-md shadow-teal-800 hover:cursor-pointer hover:bg-teal-600 hover:shadow-lg hover:shadow-teal-800 duration-300"
       onClick={buttonClickHandler}
     />
   );
